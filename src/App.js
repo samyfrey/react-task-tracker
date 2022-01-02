9f1e24144bee59d5325a5e1644b665a32684fc1c
samyfrey <samyfrey@yahoo.com>
2022-01-02 15:32:25 -0500

    hook delete task complete

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ function App() {
 			day: 'Feb 5th at 2:30pm',
 			reminder: true,
 		},
+    {
+      id: 2,
+      text: 'Make dinner',
+      day: "Mar 3rd at 7pm",
+      reminder: false
+    }
 	])
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks to Show'}
     </div>
   );
 }
