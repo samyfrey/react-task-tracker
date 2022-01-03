abe302d8cf77468a3f76b6a33f43e3f1aa3d2092
samyfrey <samyfrey@yahoo.com>
2022-01-03 16:05:51 -0500

    toggle add section complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState ([
 		{
@@ -19,19 +21,31 @@ function App() {
       reminder: false
     }
 	])
+// add Task
+// the new object newTask will contain the id newly created, and whatever is passed in the function (task) which is the text, day&time and reminder. 
+ 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+
 // delete a task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
-  // toggle reminder
+// toggle reminder
   const toggleReminder = (id) => {
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
   }
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} title="Task Tracker" />
+      {/* if showAddTask is truthy then all expression is true and AddTask is shown, otherwise nothing is shown as expression is false */}
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask}/> : 'No Tasks to Show'}
     </div>
   );
